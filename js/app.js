26ddab6f72aae098040ba4e963e7d9c67e251f1a
David Trochez <maurotrochez@gmail.com>
2014-11-10 22:58:03 -0500

    Add canvas map

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.dt = this.dt * dt;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -18,14 +19,34 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    console.log(this.sprite)
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy.png'
+}
 
+Player.prototype.update = function () {
+    
+}
 
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function (argument) {
+    console.log(argument);
+}
+
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+
+var allEnemies = [enemy1];
+var player = new Player();
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
